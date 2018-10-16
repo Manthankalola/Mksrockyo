9ad2a7f9c9ac3152b7a4b1374a538a8ba04d632c
Unknown <manthankalola1999@gmail.com>
2018-10-16 11:23:50 -0400

    assignment 3

---
+++ Manthankalola 3.py
@@ -0,0 +1,25 @@
+#print something
+print("hi i am manthan kalola and everybody call me Mksrock.")
+
+#print concinating string
+print("Bbye"+"world")
+
+#user define variables
+number=input('number=')
+print(number)
+
+#print lets get started
+print("lets get started")
+s="acadview"
+course="python"
+fees=5000
+actualfees=8000
+print("%s %s fees is %d but the actual fees is %d"%(s,course,fees,actualfees))
+
+#find the area of the circle
+radius=float(input("input radius="))
+pi=22/7
+area=pi*radius*radius
+print(area)
+
+
